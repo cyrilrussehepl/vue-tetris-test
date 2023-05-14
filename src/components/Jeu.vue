46ad5efee434a6388875b928767f47fac0a1b243
cyril <cyril.russe@student.hepl.be>
2023-05-14 18:22:36 +0200

    first commit

---
+++ src/components/Jeu.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Jeu</h1>
+  <iframe :src="gameUrl" width="800" height="800"></iframe>
+</template>
+
+<script>
+export default {
+  computed: {
+    gameUrl() {
+      return '/Tetris/index.html'
+    }
+  }
+}
+</script>
