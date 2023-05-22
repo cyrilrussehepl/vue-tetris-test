f7e1ebce89b535714693989cdd1aaedceee5c39c
Cyril Russe <cyril.russe@student.hepl.be>
2023-05-22 15:46:19 +0200

    test insertion user successfull

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,71 @@
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue'
 import Jeu from './components/Jeu.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const monFormulaire = ref(null);
+
+    onMounted(() => {
+      setupForm();
+    });
+
+    const setupForm = () => {
+      const form = document.querySelector("#formTest");
+      if (form) {
+        form.addEventListener("submit", submitForm);
+      } else {
+        console.error("Le formulaire n'a pas pu être trouvé.");
+      }
+    };
+
+    const submitForm = (event) => {
+      event.preventDefault();
+      // Logique de traitement du formulaire
+      const data = {
+    username: "Romain",
+    password: "mdp"
+  };
+
+  // Register api endpoint
+  const url = "https://europe-west1.gcp.data.mongodb-api.com/app/application-0-ptcis/endpoint/register"; 
+
+  // Effectuez la requête POST
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(result => {
+      console.log("Réponse du serveur :", result);
+      // Faites quelque chose avec la réponse du serveur
+    })
+    .catch(error => {
+      console.error("Erreur lors de la requête :", error);
+      // Gérez les erreurs de requête
+    });
+    };
+
+    return {
+      monFormulaire
+    };
+  }
+};
+
+
 </script>
 
 <template>
   <main>
-    <Jeu />
+    <form action="post" id="formTest">
+      <button type="submit">Test</button>
+    </form>
   </main>
 </template>
 
